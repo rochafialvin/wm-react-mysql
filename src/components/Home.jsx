803123d5e3dc9039cdf33bbb01f966818ee6ea62
rochafialvin <rochafi.dev@gmail.com>
2020-05-13 16:03:10 +0700

    edit and delete toggle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,34 +17,54 @@ class Home extends Component {
    getTodos = () => {
       // jika sudah login
       if(this.props.username){
-
+         // menggunakan access token untuk request ke API
          const config = { headers: { Authorization : this.props.token } }
-
+         // melakukan proses request
          axios.get(`/todo`, config)
+         // jika berhasil, data akan disimpan di state
          .then(res => this.setState({todos: res.data}))
+         // jika gagal akan menampilkan pesan error di console
          .catch(err => console.log({err}))
       }
    }
 
+   // menambahkan todo baru
    addTodo = (e) => {
+      // agar halaman tidak refresh saat di submit
       e.preventDefault()
 
+      // mempersiapkan access token dan body
       const config = { headers: { Authorization : this.props.token } }
       const body = { description : this.description.value }
 
+      // melakukan proses request untuk menambah data baru
       axios.post(`/todo`, body, config)
+         // kalau berhasil, kemudian akan request data ulang
          .then(res => this.getTodos())
+         // kalau gagal, akan munculkan di console
          .catch(err => console.log({err}))
    }
 
-   toggleTodo = (_id, _completed) => {
-      axios.patch(`/todo/${_id}`, {completed: !_completed})
+   // jika menekan tombone 'Done', mengubah status completed
+   toggleTodo = (id, completed) => {
+
+      // mempersiapkan access token dan body
+      const config = { headers: { Authorization : this.props.token } }
+      const body = {completed: !completed}
+
+      // melakukan proses request untuk mengubah status completed
+      axios.patch(`/todo/${id}`, body, config )
+         // kalau berhasil, kemudian akan request data ulang
          .then(res => this.getTodos())
-         .catch(err => console.log(err))
+         // kalau gagal, akan munculkan di console
+         .catch(err => console.log({err}))
    }
 
-   deleteTodo = (_id) => {
-      axios.delete(`/todo/${_id}`)
+   deleteTodo = (id) => {
+      // mempersiapkan access token dan body
+      const config = { headers: { Authorization : this.props.token } }
+
+      axios.delete(`/todo/${id}`, config)
          .then(res => this.getTodos())
          .catch(err => console.log(err))
    }
@@ -53,26 +73,26 @@ class Home extends Component {
       return this.state.todos.map(todo => {
          if(todo.completed){
             return(
-               <li onDoubleClick={() => this.deleteTodo(todo._id) } className="list-group-item d-flex justify-content-between">
+               <li key={todo.id} onDoubleClick={() => this.deleteTodo(todo.id) } className="list-group-item d-flex justify-content-between">
                   <span>
                      <del>{todo.description}</del>
                   </span>
    
                   <span>
-                     <input onClick={() => { this.toggleTodo(todo._id, todo.completed) }} className="btn btn-danger" type="button" value="Cancel"/>
+                     <input onClick={() => { this.toggleTodo(todo.id, todo.completed) }} className="btn btn-danger" type="button" value="Cancel"/>
                   </span>
                </li>
             )
          }
 
          return(
-            <li onDoubleClick={() => this.deleteTodo(todo._id) } className="list-group-item d-flex justify-content-between">
+            <li key={todo.id} onDoubleClick={() => this.deleteTodo(todo.id) } className="list-group-item d-flex justify-content-between">
                <span>
                   {todo.description}
                </span>
 
                <span>
-                  <input onClick={() => { this.toggleTodo(todo._id, todo.completed) }} className="btn btn-primary" type="button" value="Done"/>
+                  <input onClick={() => { this.toggleTodo(todo.id, todo.completed) }} className="btn btn-primary" type="button" value="Done"/>
                </span>
             </li>
          )
